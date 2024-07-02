47a3461599956efb9cb35af11aa518e44a89fc0a
web-luisaguilar <web.luisaguilar@gmail.com>
2024-07-02 13:54:43 -0600

    added dark/light mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './style/index.css'
-import './style/themes.css'
-import './style/custom.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
